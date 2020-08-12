d8af2f39ed0268a4c7689a939f65d22254e3bed6
FirstFallen <kovasznaibalazs@gmail.com>
2020-08-12 16:23:09 +0200

    add contenful integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,14 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `nx9cn2z55xuh`,
+        accessToken: `8z3KoFlQM93UAkh5gIfC1nuxPIzoaS0jFub-hQLEV-w`,
+      },
+    },
+  ],
 }
